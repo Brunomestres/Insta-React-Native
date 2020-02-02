becd17fb20b8b582f21c2bd0051bf3f5cc8543d6
Bruno Ribeiro <brunomestres@gmail.com>
2020-02-02 19:02:47 -0300

    Add componente Autor

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Header from './src/Components/Header';
-
+import { View, Text } from 'react-native';
+import Post from './src/Components/Post';
 export default class App extends Component {
   render()
   {
     return (
-      <Header />
+      <View>
+        <Header />
+        <Post image={require('./assets/imgs/fence.jpg')}/>
+      </View>
+
     );
   }
 }
---
+++ src/Components/Post.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Image, Dimensions } from 'react-native';
+import Autor from './Autor';
+
+export default class Post extends Component {
+    render()
+    {
+        return (
+            <View style={ styles.container }>
+                <Image source={this.props.image} style={ styles.image } />
+                <Autor email='fulano@gmail,com' nickname='fulaninho' />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+
+    },
+    image:{
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').width * 3 / 4,
+        resizeMode: 'contain' 
+    }
+});
+
